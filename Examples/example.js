f74c759c08d1446d9b3b9305f7f80be8fed15662
ObeyLordGoomy <Swaggymudkip69@gmail.com>
2020-02-26 01:05:39 -0500

    Update example.js

--- Examples/example.js
+++ Examples/example.js
@@ -1,9 +1,9 @@
-const fr = require('./FRHD.js');
+const fr = require('../FRHD.js');
 const FRHD = new fr();
 
 FRHD.login('Put account token here');
 
-FRHD.getTrackData(parseInt(args[0]), val => {
+FRHD.getTrackData(parseInt(Array.from(arguments)[0]), val => {
     if (!val.status) return console.log(`ERROR: ${val.msg}\n----------\nData: ${val.data}`);
     console.log(`Sucuess: ${val.msg}\n----------\nData: ${val.data}`)
 });
